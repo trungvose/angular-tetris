78ebd4e9fa1da4bb7cf14fee4d5e7834b31429eb
Trung Vo <trungk18@gmail.com>
2020-07-23 08:39:48 +0800

    Replace index.prod.html and set up google analytics

--- src/app/components/github/github.component.ts
+++ src/app/components/github/github.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TetrisQuery } from '@trungk18/state/tetris/tetris.query';
 import { Observable } from 'rxjs';
+import { GoogleAnalyticsService } from '@trungk18/services/google-analytics.service';
 const HASHTAG = 'angular,angulartetris,akita,typescript';
 
 @Component({
@@ -12,7 +13,7 @@ export class GithubComponent implements OnInit {
   max$: Observable<number>;
   tweetAngularTetrisUrl = `https://twitter.com/intent/tweet?url=https%3A%2F%2Fgithub.com%2Ftrungk18%2Fangular-tetris&text=Awesome%20Tetris%20game%20built%20with%20Angular%2010%20and%20Akita%2C%20can%20you%20get%20999999%20points%3F&hashtags=${HASHTAG}`;
 
-  constructor(private _query: TetrisQuery) {}
+  constructor(private _query: TetrisQuery, private _googleAnalytics: GoogleAnalyticsService) {}
 
   ngOnInit(): void {
     this.max$ = this._query.max$;
@@ -23,5 +24,13 @@ export class GithubComponent implements OnInit {
       `Woo-hoo! I got a ${max} points on Angular Tetris @tuantrungvo. Wanna join the party?`
     );
     return `https://twitter.com/intent/tweet?url=https%3A%2F%2Fgithub.com%2Ftrungk18%2Fangular-tetris&text=${text}&hashtags=${HASHTAG}`;
+  }
+
+  sendTwitterShareMaxScoreEvent() {
+    this._googleAnalytics.sendEvent('Share Twitter High Score', 'button');
+  }
+
+  sendTwitterShareEvent() {
+    this._googleAnalytics.sendEvent('Share Twitter', 'button');
   }
 }
