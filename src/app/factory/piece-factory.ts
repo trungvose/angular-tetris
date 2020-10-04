656845935f4f7a0c5e154baed7bb9b56a9632c4f
Pavel Polivka <pavel.polivka@hey.com>
2020-10-04 19:45:04 +0200

    More terris like random generator

--- src/app/factory/piece-factory.ts
+++ src/app/factory/piece-factory.ts
@@ -1,13 +1,13 @@
-import { Piece } from '../interface/piece/piece';
-import { PieceDot } from '../interface/piece/Dot';
-import { PieceI } from '../interface/piece/I';
-import { PieceJ } from '../interface/piece/J';
-import { PieceL } from '../interface/piece/L';
-import { PieceO } from '../interface/piece/O';
-import { PieceS } from '../interface/piece/S';
-import { PieceT } from '../interface/piece/T';
-import { PieceZ } from '../interface/piece/Z';
-import { Injectable } from '@angular/core';
+import {Piece} from '../interface/piece/piece';
+import {PieceDot} from '../interface/piece/Dot';
+import {PieceI} from '../interface/piece/I';
+import {PieceJ} from '../interface/piece/J';
+import {PieceL} from '../interface/piece/L';
+import {PieceO} from '../interface/piece/O';
+import {PieceS} from '../interface/piece/S';
+import {PieceT} from '../interface/piece/T';
+import {PieceZ} from '../interface/piece/Z';
+import {Injectable} from '@angular/core';
 
 export const SPAWN_POSITION_X = 4;
 export const SPAWN_POSITION_Y = -4;
@@ -17,9 +17,9 @@ export const SPAWN_POSITION_Y = -4;
 })
 export class PieceFactory {
   private _available: typeof Piece[] = [];
+  private _currentBag: typeof Piece[] = [];
 
   constructor() {
-    //this._available.push(PieceDot);
     this._available.push(PieceI);
     this._available.push(PieceJ);
     this._available.push(PieceL);
@@ -30,7 +30,24 @@ export class PieceFactory {
   }
 
   getRandomPiece(x = SPAWN_POSITION_X, y = SPAWN_POSITION_Y): Piece {
-    const idx = Math.floor(Math.random() * this._available.length);
-    return new this._available[idx](x, y);
+    if (this._currentBag.length === 0) {
+      this.generateNewBag();
+    }
+    const nextPiece = this._currentBag.pop();
+    return new nextPiece(x, y);
   }
+
+  generateNewBag() {
+    this._currentBag = this._available.slice();
+    this.shuffleArray(this._currentBag);
+  }
+
+  shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+
 }
