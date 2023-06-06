f75ce2eb9d3f35145fa006fe13f351cda4c2b387
mobi <pro97vn@gmail.com>
2023-06-06 23:04:09 +0700

    migrate ClockComponent to use signal

--- src/app/components/logo/logo.component.ts
+++ src/app/components/logo/logo.component.ts
@@ -1,54 +1,146 @@
 import { NgClass } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { concat, Observable, timer } from 'rxjs';
-import { delay, finalize, map, repeat, startWith, takeWhile, tap } from 'rxjs/operators';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  computed,
+  effect,
+  inject,
+  signal
+} from '@angular/core';
 
-@UntilDestroy()
+type RunningSide = 'r' | 'l';
+
+type LogoState = 'running' | 'blinking' | 'rest';
+
+interface RunningState {
+  count: number;
+  side: RunningSide;
+}
+
 @Component({
   selector: 't-logo',
   standalone: true,
   imports: [NgClass],
   templateUrl: './logo.component.html',
-  styleUrls: ['./logo.component.scss']
+  styleUrls: ['./logo.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LogoComponent implements OnInit {
-  className = '';
+export class LogoComponent {
+  private runState = signal<RunningState>({
+    count: 1,
+    side: 'r'
+  });
+  private blinkingCount = signal(1);
+  private mode = signal<LogoState>('running');
 
-  ngOnInit(): void {
-    concat(this.run(), this.eyes())
-      .pipe(delay(5000), repeat(1000), untilDestroyed(this))
-      .subscribe();
-  }
+  className = computed(() => {
+    switch (this.mode()) {
+      case 'running': {
+        return this.getRunningClassName();
+      }
+      default: {
+        return this.getBlinkingClassName();
+      }
+    }
+  });
 
-  eyes() {
-    return timer(0, 500).pipe(
-      startWith(0),
-      map((x) => x + 1),
-      takeWhile((x) => x < 6),
-      tap((x) => {
-        const state = x % 2 === 0 ? 1 : 2;
-        this.className = `l${state}`;
-      })
-    );
-  }
+  private intervalRef = null;
+  private setTimeoutRef = null;
 
-  run(): Observable<number> {
-    let side = 'r';
-    return timer(0, 100).pipe(
-      startWith(0),
-      map((x) => x + 1),
-      takeWhile((x) => x <= 40),
-      tap((x) => {
-        if (x === 10 || x === 20 || x === 30) {
-          side = side === 'r' ? 'l' : 'r';
+  constructor() {
+    effect(
+      () => {
+        switch (this.mode()) {
+          case 'running': {
+            if (!this.intervalRef) {
+              this.intervalRef = this.startRunning();
+            }
+
+            const { count } = this.runState();
+
+            if (count > 40) {
+              this.clearInterval();
+              this.mode.set('blinking');
+              this.runState.set({
+                count: 1,
+                side: 'r'
+              });
+            }
+            break;
+          }
+          case 'blinking': {
+            if (!this.intervalRef) {
+              this.intervalRef = this.startBlinking();
+            }
+
+            if (this.blinkingCount() >= 6) {
+              this.clearInterval();
+              this.mode.set('rest');
+              this.blinkingCount.set(1);
+            }
+            break;
+          }
+          case 'rest': {
+            this.setTimeoutRef = setTimeout(() => {
+              this.mode.set('running');
+            }, 5000);
+            break;
+          }
         }
-        const state = x % 2 === 0 ? 3 : 4;
-        this.className = `${side}${state}`;
-      }),
-      finalize(() => {
-        this.className = `${side}1`;
-      })
+      },
+      { allowSignalWrites: true }
     );
+
+    inject(DestroyRef).onDestroy(() => {
+      if (this.intervalRef) {
+        clearInterval(this.intervalRef);
+      }
+
+      if (this.setTimeoutRef) {
+        clearTimeout(this.setTimeoutRef);
+      }
+    });
+  }
+
+  private startBlinking() {
+    return setInterval(() => {
+      this.blinkingCount.update((count) => count + 1);
+    }, 500);
+  }
+
+  private startRunning() {
+    return setInterval(() => {
+      this.runState.update(({ count, side }) => {
+        const newCount = count + 1;
+
+        return {
+          count: newCount,
+          side:
+            newCount === 10 || newCount === 20 || newCount === 30
+              ? side === 'r'
+                ? 'l'
+                : 'r'
+              : side
+        };
+      });
+    }, 100);
+  }
+
+  private getRunningClassName(): string {
+    const { count, side } = this.runState();
+    const state = count === 41 ? 1 : count % 2 === 0 ? 3 : 4;
+    return `${side}${state}`;
+  }
+
+  private getBlinkingClassName() {
+    const state = this.blinkingCount() % 2 === 0 ? 1 : 2;
+
+    return `l${state}`;
+  }
+
+  private clearInterval() {
+    clearInterval(this.intervalRef);
+    this.intervalRef = null;
   }
 }
