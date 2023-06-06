744bba55d547e0f5db15b7fa7c23d216271c142b
mobi <pro97vn@gmail.com>
2023-06-06 23:04:28 +0700

    migrate PauseComponent to use signal

--- src/app/components/pause/pause.component.ts
+++ src/app/components/pause/pause.component.ts
@@ -1,26 +1,50 @@
-import { AsyncPipe } from '@angular/common';
-import { Component } from '@angular/core';
 import { GameState } from '@angular-tetris/interface/game-state';
-import { TetrisQuery } from '@angular-tetris/state/tetris/tetris.query';
-import { interval, Observable, of } from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
+import { TetrisStateService } from '@angular-tetris/state/tetris/tetris.state';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  effect,
+  inject,
+  signal
+} from '@angular/core';
 
 @Component({
   selector: 't-pause',
   standalone: true,
-  imports: [AsyncPipe],
   templateUrl: './pause.component.html',
-  styleUrls: ['./pause.component.scss']
+  styleUrls: ['./pause.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PauseComponent {
-  paused$: Observable<boolean> = this.query.gameState$.pipe(
-    switchMap((state) => {
-      if (state === GameState.Paused) {
-        return interval(250).pipe(map((num) => !!(num % 2)));
-      }
-      return of(false);
-    })
-  );
+  private tetrisState = inject(TetrisStateService);
+  private timerRef: number;
+  paused = signal(false);
 
-  constructor(private query: TetrisQuery) {}
+  constructor() {
+    effect(
+      () => {
+        if (this.timerRef) {
+          clearInterval(this.timerRef);
+        }
+
+        if (this.tetrisState.gameState() === GameState.Paused) {
+          this.timerRef = setInterval(() => {
+            this.paused.update((pausedValue) => !pausedValue);
+          }, 250);
+
+          return;
+        }
+
+        this.paused.set(false);
+      },
+      { allowSignalWrites: true }
+    );
+
+    inject(DestroyRef).onDestroy(() => {
+      if (this.timerRef) {
+        clearInterval(this.timerRef);
+      }
+    });
+  }
 }
